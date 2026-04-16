7888431c8fd51febbd05c4e8379008adae41aca2
dmitriy18sd <dmstoanov10@gmail.com>
2026-04-16 19:08:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 const { PeerServer } = require('peer');
-const peerServer = PeerServer({ port: process.env.PORT || 9000, path: '/' });
+const peerServer = PeerServer({
+    port: process.env.PORT || 9000,
+    path: '/'
+});
 console.log('PeerJS сервер запущен на порту', process.env.PORT || 9000);
